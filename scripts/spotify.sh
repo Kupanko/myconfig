069cc18fa519702131ee3ecc70d2e1fc9b6dbf69
Kupano <24877137+Kupanko@users.noreply.github.com>
2025-12-27 14:51:11 +0300

    Fix spotify.sh

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -2,13 +2,16 @@
 
 OUTPUT_FILE="/tmp/spotify_status.txt"
 
-playerctl metadata -F -f -p=spotify '{{status}};{{artist}} - {{title}}' | while read -r line; do
-    STATUS=$(echo "$line" | cut -d';' -f1)
-    METADATA=$(echo "$line" | cut -d';' -f2-)
+playerctl --player="spotify" metadata --follow --format '{{playerName}};{{status}};{{artist}} - {{title}}' | while read -r line; do
+    PLAYER=$(echo "$line" | cut -d';' -f1)
+    STATUS=$(echo "$line" | cut -d';' -f2)
+    METADATA=$(echo "$line" | cut -d';' -f3-)
 
-    if [ "$STATUS" = "Playing" ]; then
-        echo "$METADATA" > "$OUTPUT_FILE"
-    else
-        echo "Empty" > "$OUTPUT_FILE"
+    if [ "$PLAYER" = "spotify" ]; then
+        if [ "$STATUS" = "Playing" ]; then
+            echo "$METADATA" > "$OUTPUT_FILE"
+        else
+            echo "Empty" > "$OUTPUT_FILE"
+        fi
     fi
 done
